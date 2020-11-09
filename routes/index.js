31efe33a724c78eec784585191378553fe56be6d
mukhindev <mukhin.dev@gmail.com>
2020-11-10 00:17:48 +0300

    hotfix: изменить ограничение поля name в запросе

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-    name: Joi.string().required().min(8).max(30),
+    name: Joi.string().required().min(2).max(30),
   }),
 }), createUser);
 
